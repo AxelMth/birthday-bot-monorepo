2f68e385c32144fd44afa9a301f989e24f6357f7
AxelMth <aujoulator@hotmail.fr>
2025-02-16 15:26:19 +0100

    rename column

--- packages/backend/src/db/schema.ts
+++ packages/backend/src/db/schema.ts
@@ -3,5 +3,5 @@ import { date, integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 export const usersTable = pgTable('users', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  birthDateAsDate: date().notNull().default('now()'),
+  birthDate: date().notNull().default('now()'),
 });
