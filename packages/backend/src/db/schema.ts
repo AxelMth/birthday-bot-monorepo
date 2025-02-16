cda3e9cb3c2e17acf693052bb1f9e071eba3fdcd
AxelMth <aujoulator@hotmail.fr>
2025-02-16 15:26:02 +0100

    remove one column

--- packages/backend/src/db/schema.ts
+++ packages/backend/src/db/schema.ts
@@ -3,6 +3,5 @@ import { date, integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 export const usersTable = pgTable('users', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  birthDate: varchar({ length: 10 }).notNull(),
   birthDateAsDate: date().notNull().default('now()'),
 });
