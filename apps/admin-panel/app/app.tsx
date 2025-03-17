1bb35e1e97e694ef9de2598ad5f741931373dbbf
AxelMth <aujoulator@hotmail.fr>
2025-03-17 15:25:57 +0100

    add table

--- apps/admin-panel/app/app.tsx
+++ apps/admin-panel/app/app.tsx
@@ -1,12 +1,40 @@
-import { Button, HStack } from '@chakra-ui/react';
-import { useState } from 'react';
+import { For, Stack, Table, Container } from '@chakra-ui/react';
 
 export default function App() {
-  const [count, setCount] = useState(0);
   return (
-    <HStack>
-      <p>Count: {count}</p>
-      <Button onClick={() => setCount((prev) => prev + 1)}>Click me</Button>
-    </HStack>
+    <Container maxW="container.xl">
+      <Stack gap="10">
+        <For each={['sm', 'md', 'lg']}>
+          {(size) => (
+            <Table.Root key={size} size={size}>
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColumnHeader>Product</Table.ColumnHeader>
+                  <Table.ColumnHeader>Category</Table.ColumnHeader>
+                  <Table.ColumnHeader textAlign="end">Price</Table.ColumnHeader>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {items.map((item) => (
+                  <Table.Row key={item.id}>
+                    <Table.Cell>{item.name}</Table.Cell>
+                    <Table.Cell>{item.category}</Table.Cell>
+                    <Table.Cell textAlign="end">{item.price}</Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table.Root>
+          )}
+        </For>
+      </Stack>
+    </Container>
   );
 }
+
+const items = [
+  { id: 1, name: 'Laptop', category: 'Electronics', price: 999.99 },
+  { id: 2, name: 'Coffee Maker', category: 'Home Appliances', price: 49.99 },
+  { id: 3, name: 'Desk Chair', category: 'Furniture', price: 150.0 },
+  { id: 4, name: 'Smartphone', category: 'Electronics', price: 799.99 },
+  { id: 5, name: 'Headphones', category: 'Accessories', price: 199.99 },
+];
