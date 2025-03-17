3c8beff5757e07d15428a93d11725ad422e720c6
AxelMth <aujoulator@hotmail.fr>
2025-03-17 14:54:34 +0100

    make request to backend

--- apps/admin-panel/app/app.tsx
+++ apps/admin-panel/app/app.tsx
@@ -1,5 +1,13 @@
+import { useEffect, useState } from 'react';
+
 export function App() {
-  return <div>Salut</div>;
+  const [data, setData] = useState<string>('');
+  useEffect(() => {
+    fetch(import.meta.env.VITE_SERVER_URL as string)
+      .then((res) => res.json())
+      .then((data) => setData(JSON.stringify(data, null, 4)));
+  });
+  return <div>{data}</div>;
 }
 
 export default App;
