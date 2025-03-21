659ac6e2874be84be92772f3b45b8fd2a91676c0
AxelMth <aujoulator@hotmail.fr>
2025-03-21 17:18:41 +0100

    update interface

--- apps/fastify-api/src/application/ports/output/communication.repository.ts
+++ apps/fastify-api/src/application/ports/output/communication.repository.ts
@@ -2,4 +2,9 @@ import { Communication } from '../../../domain/entities/communication';
 
 export interface CommunicationRepository {
   getByPersonId(personId: number): Promise<Communication[]>;
+  createCommunication(communication: Communication): Promise<Communication>;
+  updateCommunicationById(
+    id: number,
+    communication: Communication
+  ): Promise<void>;
 }
