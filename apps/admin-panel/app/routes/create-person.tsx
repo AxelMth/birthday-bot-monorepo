9355f6bcb362501e8cbda13306f58fed6a4b8c2c
AxelMth <aujoulator@hotmail.fr>
2025-03-21 17:01:27 +0100

    create person form

---
+++ apps/admin-panel/app/routes/create-person.tsx
@@ -0,0 +1,110 @@
+import { useNavigate } from 'react-router';
+import {
+  Button,
+  Container,
+  Field,
+  Fieldset,
+  For,
+  Input,
+  NativeSelect,
+  Stack,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+
+import { peopleClient } from '../lib/client';
+
+interface Person {
+  id: number;
+  name: string;
+  birthdate: Date;
+  communications: { application: string }[];
+}
+
+export default function CreatePersonComponent() {
+  const [person, setPerson] = useState<Person>({
+    id: 0,
+    name: '',
+    birthdate: new Date(),
+    communications: [{ application: 'slack' }],
+  });
+
+  const navigate = useNavigate();
+  const createPersonById = (personId: number, person: Person) => {
+    peopleClient
+      .createPerson({
+        body: person,
+      })
+      .then((response) => {
+        if (response.status !== 200) {
+          return;
+        }
+        navigate(`/`);
+      });
+  };
+
+  return (
+    <Container fluid centerContent>
+      <Fieldset.Root size="lg" maxW="md">
+        <Stack>
+          <Fieldset.Legend>Création d'un anniversaire</Fieldset.Legend>
+        </Stack>
+
+        <Fieldset.Content>
+          <Field.Root>
+            <Field.Label>Nom</Field.Label>
+            <Input
+              name="name"
+              value={person.name}
+              onChange={(e) =>
+                setPerson({
+                  ...person,
+                  name: e.target.value,
+                })
+              }
+            />
+          </Field.Root>
+
+          <Field.Root>
+            <Field.Label>Date de naissance</Field.Label>
+            <Input
+              name="date"
+              type="date"
+              value={`${person.birthdate.toISOString().split('T')[0]}`}
+              onChange={(e) => {
+                const date = new Date(e.target.value);
+                setPerson({
+                  ...person,
+                  birthdate: date,
+                });
+              }}
+            />
+          </Field.Root>
+
+          <Field.Root>
+            <Field.Label>Application</Field.Label>
+            <NativeSelect.Root>
+              <NativeSelect.Field name="application">
+                <For each={['slack']}>
+                  {(item) => (
+                    <option key={item} value={item}>
+                      {item}
+                    </option>
+                  )}
+                </For>
+              </NativeSelect.Field>
+              <NativeSelect.Indicator />
+            </NativeSelect.Root>
+          </Field.Root>
+        </Fieldset.Content>
+
+        <Button
+          type="submit"
+          alignSelf="flex-start"
+          onClick={() => createPersonById(person.id, person)}
+        >
+          Modifier
+        </Button>
+      </Fieldset.Root>
+    </Container>
+  );
+}
